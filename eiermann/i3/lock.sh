dd7125c72a82a27d5998e4505a8a38158eb4e7c8
Falko Riemenschneider <falko.riemenschneider@arcor.de>
2020-04-06 21:35:56 +0200

    Compose lock image with wallpaper for lockscreen

--- eiermann/i3/lock.sh
+++ eiermann/i3/lock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Depends on: convert, i3-lock, compton
+# Depends on: convert, composite, i3lock, compton
 
 # Randomy picks a wallpaper file from ~/Pitcures/Wallpapers
 # converts it to the current screen resolution and starts i3-lock
@@ -10,15 +10,18 @@ geometry=`xdpyinfo | grep dimensions | sed -n 's/.*dimensions:\ \+\([0-9]\+\x[0-
 wallpapers=(~/Pictures/Wallpapers/*)
 
 wallpaper=${wallpapers[RANDOM % ${#wallpapers[@]}]}
+lock=~/.config/i3/lock.png
+locked_wallpaper=/tmp/i3-lockscreen.png
 
 echo "Wallpaper file $wallpaper"
 
 lock() {
-  i3lock -n -i /tmp/i3-lockscreen.png
-  compton -Gf -b --vsync opengl
+  i3lock -n -i "$locked_wallpaper"
+  compton -Gf -b -i 0.9
 }
 
-convert $wallpaper -resize $geometry! /tmp/i3-lockscreen.png
+convert $wallpaper -resize $geometry! "$locked_wallpaper"
+composite "$lock" "$locked_wallpaper"  "$locked_wallpaper"
 lock &
 sleep 1
 killall compton
