2a75037b4be943cf1b7a2dbf774b91085044af38
Falko Riemenschneider <riemenschneider@doctronic.de>
2020-04-28 21:23:51 +0200

    Remove GB layout fron layout cycling scripts

--- hadid/i3/cycle-kbd-layout.sh
+++ hadid/i3/cycle-kbd-layout.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
 de="de(nodeadkeys)"
-gb="gb"
 us="us(altgr-intl)"
 
 current_layout=`setxkbmap -query | grep layout | sed -n 's/layout:\s*\(.*\)/\1/p'`
 next_layout="$de"
 
 if [[ "$current_layout" == "$de" ]]; then
-    next_layout="$gb"
-fi
-
-if [[ "$current_layout" == "$gb" ]]; then
     next_layout="$us"
 fi
 
