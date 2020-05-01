a3608ec8d6c4e0ed957c01b00fc3005d5028cfd7
Falko Riemenschneider <riemenschneider@doctronic.de>
2020-05-01 21:52:19 +0200

    Show sound volume in status bar

---
+++ eiermann/i3/blocks/pulseaudio.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+active_sink=$(pacmd list-sinks | grep "state\: RUNNING" -B4 -A7 | grep "index:\|name:\|volume: \(front\|mono\)\|muted:")
+muted=$(echo "$active_sink" | grep muted | sed -n 's/.*:\s\(.*\)/\1/p')
+volume=$(echo "$active_sink" | grep volume | sed -n 's/.*\s\([0-9]*%\).*/\1/p')
+
+if [[ -z "$active_sink" ]] ; then
+    echo ""
+elif [[ "$muted" == "no" ]] ; then
+    echo $volume
+else
+    echo "Mute"
+fi
