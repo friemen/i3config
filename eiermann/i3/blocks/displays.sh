7eb1ec02211b07532b514ffdd8713b4b75ca82bc
Falko Riemenschneider <riemenschneider@doctronic.de>
2020-05-02 22:25:00 +0200

    Add display status to i3 blocks

---
+++ eiermann/i3/blocks/displays.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#all_displays=$(xrandr | grep -e "connected" | sed -n 's/\([^\s]\)\s.*/\1/p' | tr '\n' ' ')
+connected_displays=$(xrandr | grep -e " connected" | sed -n 's/\([^\s]\)\s.*/\1/p' | tr '\n' ' ')
+
+internal_display_name="eDP1"
+internal_display_symbol=
+external_display_symbol=
+
+output=""
+for display in $connected_displays ; do
+    if [[ $display == $internal_display ]] ; then
+        output="$output $internal_display_symbol  $display"
+    else
+        output="$output $external_display_symbol  $display"
+    fi
+done
+echo "$output"
