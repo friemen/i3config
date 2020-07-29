21463543268d1f2013b245f30ef5edd05277edff
Falko Riemenschneider <riemenschneider@doctronic.de>
2020-07-29 11:04:36 +0200

    Fix CPU usage calculation

--- eiermann/i3/blocks/cpu_usage.sh
+++ eiermann/i3/blocks/cpu_usage.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-output=`mpstat -P all 1 1 | tail -n 1 | sed -n 's/.*all\ *\([0-9]\+\).*/\1/p'`
-echo "$output%"
+idle=`mpstat -P all 1 1 | tail -n 1 | sed -n 's/.*\s\([0-9,.]\+\)$/\1/p'`
+idle=`echo $idle | sed -n 's/,/./p'`
+usage=`echo "scale=2;100.00-$idle" | bc`
+echo "$usage%"
