03d19a619bbe6613069844eeb9e30bd89b8e773a
Falko Riemenschneider <riemenschneider@doctronic.de>
2021-12-17 20:56:02 +0100

    Improve on how the active audio sink is found

--- eiermann/i3/blocks/pulseaudio.sh
+++ eiermann/i3/blocks/pulseaudio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-active_sink=$(pacmd list-sinks | grep "state\: RUNNING" -B4 -A7 | grep "index:\|name:\|volume: \(front\|mono\)\|muted:")
+active_sink=$(pacmd list-sinks | grep "state\: RUNNING" -m 1 -B4 -A7 | grep "index:\|name:\|volume: \(front\|mono\)\|muted:")
 muted=$(echo "$active_sink" | grep muted | sed -n 's/.*:\s\(.*\)/\1/p')
 volume=$(echo "$active_sink" | grep volume | sed -n 's/.*\s\([0-9]*%\).*/\1/p')
 
