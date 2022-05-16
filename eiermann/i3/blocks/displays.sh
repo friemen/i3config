23c60e9bbfc871826498f52f9a840a043ce1cafc
Falko Riemenschneider <riemenschneider@doctronic.de>
2022-05-16 20:27:56 +0200

    Reduce output when no external display is present

--- eiermann/i3/blocks/displays.sh
+++ eiermann/i3/blocks/displays.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-#all_displays=$(xrandr | grep -e "connected" | sed -n 's/\([^\s]\)\s.*/\1/p' | tr '\n' ' ')
 connected_displays=$(xrandr | grep -e " connected" | sed -n 's/\([^\s]\)\s.*/\1/p' | tr '\n' ' ')
-
+display_number=$(echo "$connected_displays" | wc -w)
 internal_display_name="eDP-1-1"
 internal_display_symbol=""
 external_display_symbol=""
 
 
-output=""
-for display in $connected_displays ; do
-    # echo $display
-    if [[ "$display" == "$internal_display_name" ]] ; then
-        output="$output $internal_display_symbol $display"
-    else
-        output="$output $external_display_symbol $display"
-    fi
-done
-echo "$output"
+if [ $display_number -gt 1 ]; then
+    for display in $connected_displays ; do
+        if [ "$display" == "$internal_display_name" ] ; then
+            output="$internal_display_symbol $display"
+        else
+            output="$output   $external_display_symbol $display"
+        fi
+    done
+    echo "$output"
+else
+    echo "$internal_display_symbol 1"
+fi
