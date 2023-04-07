a484dbb3dcb9b94f819e7324e97d575a683f8110
Falko Riemenschneider <riemenschneider@doctronic.de>
2023-04-07 14:35:11 +0200

    Improve CPU usage block

--- eiermann/i3/blocks/cpu_usage.sh
+++ eiermann/i3/blocks/cpu_usage.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-idle=`mpstat -P all 1 1 | tail -n 1 | sed -n 's/.*\s\([0-9,.]\+\)$/\1/p'`
+idle=$(mpstat -P all 1 1 | tail -n 1 | sed -n 's/.*\s\([0-9,.]\+\)$/\1/p')
 # the output of mpstat depends on LC_MEASUREMENT
 # use commands locale and update-locale to set these to german defaults
-idle=`echo $idle | sed -n 's/,/./p'`
-usage=`echo "scale=2;100.00-$idle" | bc`
+idle=$(echo $idle | sed -n 's/,/./p')
+usage=$(echo "v=100-$idle;if (v < 1) 0 else v" | bc | sed -e 's/\(.*\)\..*/\1/')
 echo "$usage%"
