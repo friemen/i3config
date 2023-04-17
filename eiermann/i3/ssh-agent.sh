b6a07a14bb6ab4ae1bfedd7ddb592b97d9c8aa6b
Falko Riemenschneider <riemenschneider@doctronic.de>
2023-04-17 21:01:53 +0200

    ssh-agent is supposed to be started via .profile gnome-keyring-daemon

--- eiermann/i3/ssh-agent.sh
+++ eiermann/i3/ssh-agent.sh
@@ -2,4 +2,5 @@
 
 # eval $(/usr/bin/gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh)
 # export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
+killall ssh-agent
 eval $(ssh-agent -s)
