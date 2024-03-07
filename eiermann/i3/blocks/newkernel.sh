bab7148ff0e94692c4e14bc7967163c0b420a4f5
Falko Riemenschneider <riemenschneider@doctronic.de>
2024-03-07 20:01:50 +0100

    Fix kernel sorting

--- eiermann/i3/blocks/newkernel.sh
+++ eiermann/i3/blocks/newkernel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-installed_kernels=$(for f in $(ls /boot); do echo $f | sed -n -E 's/System.map-(.*)/\1/p'; done)
+installed_kernels=$(for f in $(ls -t /boot); do echo $f | sed -n -E 's/System.map-(.*)/\1/p'; done)
 number_of_installed_kernels=$(echo "$installed_kernels" | wc -l)
-latest_available_kernel=$(echo "$installed_kernels" | tail -n 1)
+latest_available_kernel=$(echo "$installed_kernels" | head -n 1)
 
 active_kernel=$(uname -r)
 
