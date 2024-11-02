c2f05f5636f746e5d1dae4cf5af92a75e3ecfde8
Falko Riemenschneider <riemenschneider@doctronic.de>
2024-11-02 08:06:47 +0100

    Improve keyboard detection

--- eiermann/i3/on-inputplug.sh
+++ eiermann/i3/on-inputplug.sh
@@ -10,13 +10,19 @@ type=$3
 shift 3
 description="$*"
 
+vendorid="bb80" # see info.json of friemen keyboard definitions
 
-#notify-send -t 2000 "$event $type $description"
+#notify-send -t 10000 "$event $type $description"
 
-if echo "$event $type $description" | grep -q "XIDeviceEnabled XISlaveKeyboard.*System Control"; then
-    $scriptdir/setup-kbd.sh "us(altgr-intl)"
-fi
+keyboard_enabled=$(echo "$event $type $description" | grep "XIDeviceEnabled XISlaveKeyboard.*System Control")
+keyboard_disabled=$(echo "$event $type" | grep "XIDeviceDisabled XISlaveKeyboard")
+handwired=$(lsusb | sed -n 's/.*ID \(.\{4\}:.\{4\}\) .*/\1/p' | grep $vendorid)
 
-if echo "$event $type" | grep -q "XIDeviceDisabled XISlaveKeyboard"; then
-    $scriptdir/setup-kbd.sh "de(nodeadkeys)"
+
+if [[ -n "$keyboard_enabled" || -n "$keyboard_disabled" ]]; then
+    if [[ -n "$handwired" ]]; then
+        $scriptdir/setup-kbd.sh "us(altgr-intl)"
+    else
+        $scriptdir/setup-kbd.sh "de(nodeadkeys)"
+    fi
 fi
